cf9e508fea7b1195d009eff584b9d216983ac00b
Samiransetua1234 <86732606+Samiransetua1234@users.noreply.github.com>
2023-05-20 22:14:42 +0530

    pattern-3

---
+++ pattern-3/AlphabetPattern8.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class AlphabetPattern8{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++){
+			char k = 'A';
+			for(int j = 1; j <= n-i; j++){
+				System.out.print("  ");
+			}
+			for(int j = 1; j <= i; j++){
+				System.out.print(k++ +"   ");
+			}
+			
+			System.out.println();
+		}
+	}
+}
